711ee08bba406e90aec004898a9563966edcb62e
Shivam <100505778+itssrokay@users.noreply.github.com>
2024-10-07 19:15:28 +0530

    Update data.js

--- reminder-frontends/src/data.js
+++ reminder-frontends/src/data.js
@@ -1,4 +1,4 @@
-export const url = 'https://reminder-backend-tdlh.onrender.com';
+// export const url = 'https://reminder-backend-tdlh.onrender.com';
 // export const url = 'https://medicine-reminder-application.onrender.com';
-// export const url='http://localhost:9000';
+export const url='http://localhost:9000';
 
